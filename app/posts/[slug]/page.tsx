8aa64cde2da206bac3751a901dcacc314615cbe2
CatAvadani <catalinaavadani@gmail.com>
2024-03-11 12:00:41 +0100

    create the routing for the static

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,6 +2,15 @@ import fs from "fs/promises";
 
 type PageProps = { params: { slug: string } };
 
+export async function generateStaticParams() {
+  const data = await fs.readFile("data/blog-posts.json", {
+    encoding: "utf-8",
+  });
+  const blogPosts = JSON.parse(data);
+
+  return blogPosts.map((post: any) => ({ slug: post.slug }));
+}
+
 export default async function BlogPostPage({ params }: PageProps) {
   const data = await fs.readFile("data/blog-posts.json", {
     encoding: "utf-8",
@@ -18,10 +27,10 @@ export default async function BlogPostPage({ params }: PageProps) {
     );
   }
   return (
-    <main>
-      <h1 className=' text-3xl'>{params.slug}</h1>
-      <div className=' text-gray-600 mt-4'>
-        <p className=' text-2xl'>{blogPost.author}</p>
+    <main className='flex flex-col gap-2 p-8'>
+      <h1 className=' text-3xl mb-2'>{params.slug}</h1>
+      <div className=' flex flex-col text-gray-600 gap-2'>
+        <p className=' text-2xl mb-4'>{blogPost.author}</p>
         <p className=' text-gray-600'>{new Date().toUTCString()}</p>
       </div>
       <p className='text-2xl'>{blogPost.content}</p>
